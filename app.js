02acdc0bb6809745992d6f8e54a9ebcaa9ef39fa
Uladzimir Barysenka <vovaborisenko@live.com>
2022-02-06 15:48:03 +0300

    adds some config settings

--- app.js
+++ app.js
@@ -7,45 +7,45 @@ const PORT = 3000;
 const createPath = (name) => path.resolve(__dirname, 'views', `${name}.html`);
 
 const server = http.createServer((req, res) => {
-    console.log(`Server request ${req.method} ${req.url}`);
+  console.log(`Server request ${req.method} ${req.url}`);
 
-    res.setHeader('Content-Type', 'text/html');
-    
-    let basePath = '';
+  res.setHeader('Content-Type', 'text/html');
 
-    switch (req.url){
-        case '/':
-        case '/home':
-        case '/index.html':
-            basePath = 'index';
-            res.statusCode = 200;
-            break;
-        case '/about-us':
-            res.statusCode = 301;
-            res.setHeader('Location', '/contacts');
-            break;
-        case '/contacts':
-            basePath = 'contacts';
-            res.statusCode = 200;
-            break;
-        default:
-            basePath = 'error';
-            res.statusCode = 404;
+  let basePath = '';
+
+  switch (req.url) {
+    case '/':
+    case '/home':
+    case '/index.html':
+      basePath = 'index';
+      res.statusCode = 200;
+      break;
+    case '/about-us':
+      res.statusCode = 301;
+      res.setHeader('Location', '/contacts');
+      break;
+    case '/contacts':
+      basePath = 'contacts';
+      res.statusCode = 200;
+      break;
+    default:
+      basePath = 'error';
+      res.statusCode = 404;
+  }
+
+  fs.readFile(createPath(basePath), (err, data) => {
+    if (err) {
+      console.log('Error: ', err);
+    } else {
+      res.write(data);
     }
 
-    fs.readFile(createPath(basePath), (err, data) => {
-        if (err) {
-            console.log('Error: ', err);
-        } else {
-            res.write(data);
-        }
-
-        res.end();
-    })
-})
+    res.end();
+  });
+});
 
 server.listen(PORT, (err) => {
-    if (err) return console.log('Error: ', err);
+  if (err) return console.log('Error: ', err);
 
-    console.log(`Server listening: http://localhost:${PORT}`);
-})
+  return console.log(`Server listening: http://localhost:${PORT}`);
+});
