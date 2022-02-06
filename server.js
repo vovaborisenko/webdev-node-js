9cf4d13c90224a9821f1e5735c56169d747b7b15
Uladzimir Barysenka <vovaborisenko@live.com>
2022-02-06 18:56:13 +0300

    adds templating language ejs

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 const PORT = 3000;
 
-const createPath = (name) => path.resolve(__dirname, 'views', `${name}.html`);
+const createPath = (name) => path.resolve(__dirname, 'ejs', `${name}.ejs`);
 
 app.listen(PORT, (err) => {
   if (err) return console.log('Error: ', err);
@@ -14,11 +16,19 @@ app.listen(PORT, (err) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(createPath('index'));
+  const title = 'Home'
+  res.render(createPath('index'), { title });
 });
 
 app.get('/contacts', (req, res) => {
-  res.sendFile(createPath('contacts'));
+  const title = 'Contacts';
+  const contacts = [
+    { link: 'http://youtube.com/YauhenKavalchuk', label: 'YouTube' },
+    { link: 'http://github.com/YauhenKavalchuk', label: 'Twitter' },
+    { link: 'http://twitter.com/YauhenKavalchuk', label: 'GitHub' },
+  ];
+
+  res.render(createPath('contacts'), { contacts, title });
 });
 
 app.get('/about-us', (req, res) => {
@@ -26,17 +36,27 @@ app.get('/about-us', (req, res) => {
 });
 
 app.get('/posts/:id', (req, res) => {
-  res.sendFile(createPath('post'));
+  const title = 'Post';
+
+  res.render(createPath('post'), { title });
 });
 
 app.get('/posts', (req, res) => {
-  res.sendFile(createPath('posts'));
+  const title = 'Post list';
+
+  res.render(createPath('posts'), { title });
 });
 
 app.get('/add-post', (req, res) => {
-  res.sendFile(createPath('add-post'));
+  const title = 'New post';
+
+  res.render(createPath('add-post'), { title });
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile(createPath('error'));
+  const title = 'Oops! Error';
+
+  res
+    .status(404)
+    .render(createPath('error'), { title });
 });
